c4158aa29f4a5c6d4200281356b9e5ea194e29fd
mixelas <michelakisgio@gmail.com>
2026-07-01 04:59:51 +0300

    feat: enhance TestCase setup with conditional Vite handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (method_exists($this, 'withoutVite')) {
+            $this->withoutVite();
+        }
+    }
 }
